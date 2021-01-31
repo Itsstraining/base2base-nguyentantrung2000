6ca2d6440d7d3a7da627f3b2ba804abb24d8b4a4
trung <trung.nt4996@sinhvien.hoasen.edu.vn>
2021-01-31 17:15:43 +0700

    1

--- program.js
+++ program.js
@@ -1,8 +1,38 @@
 // Add new functions, variables here
 
 function main(input) {
-  // Your code goes here
-  // Using console.log to answer the question
+  let inp = input.split(' ');
+  let n = parseInt(inp[0]);
+  let x = parseInt(inp[1]);
+  let y = parseInt(inp[2]);
+  Change(n, x, y)
 }
+function Change(n, x, y) {
+    let result;
+    switch (n,x,y) {
+      case (x == 10 && y == 2):
+        {
+          result= n % y;
+          console.log(result);
+          break;
+        }
+      case (x == 8 && y == 10):
+        {
+
+          break;
+        }
+      case (x == 2 && y == 16):
+        {
+
+          break;
+        }
+      default:
+        {
+
+        }
+    }
+  }
+
+
 
 module.exports = main;
