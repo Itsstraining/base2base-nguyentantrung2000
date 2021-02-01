da630fe877151e7c8585c6033e1fb5f095569977
trung <trung.nt4996@sinhvien.hoasen.edu.vn>
2021-02-01 10:58:49 +0700

    2

--- program.js
+++ program.js
@@ -5,33 +5,24 @@ function main(input) {
   let n = parseInt(inp[0]);
   let x = parseInt(inp[1]);
   let y = parseInt(inp[2]);
-  Change(n, x, y)
+  console.log(ThapphanSangNhi(n, x, y))
 }
-function Change(n, x, y) {
-    let result;
-    switch (n,x,y) {
-      case (x == 10 && y == 2):
-        {
-          result= n % y;
-          console.log(result);
-          break;
-        }
-      case (x == 8 && y == 10):
-        {
-
-          break;
-        }
-      case (x == 2 && y == 16):
-        {
-
-          break;
-        }
-      default:
-        {
-
-        }
-    }
+function ThapphanSangNhi(n, x, y) {
+  let remainder;
+  let arr = [];
+  while (n != 0) {
+    remainder = n % 2;
+    n = Math.floor(n / 2);
+    arr.unshift(remainder);
   }
+  return arr.join('')
+}
+function NhiphanSangthapphan()
+{
+    
+}
+
+
 
 
 
